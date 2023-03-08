34a9b1df2a8a0ea29ab72f4741858795b34caae1
Aleksandar Lalov <alex.lalov04@gmail.com>
2023-03-08 10:34:13 +0200

    Adding details page

---
+++ Kanban/Controllers/CommentController.cs
@@ -0,0 +1,99 @@
+﻿using Kanban.Data;
+using Kanban.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kanban.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        private readonly UserManager<User> _userManager;
+        public CommentController(ApplicationDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: CommentController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CommentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CommentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CommentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(int id, Comment comment)
+        {
+            try
+            {
+                comment.UserId = _userManager.GetUserId(User);
+                comment.TaskId = id;
+                _context.Add(comment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Task");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CommentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CommentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
