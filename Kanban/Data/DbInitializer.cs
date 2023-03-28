ef400c6aeb11c928bf5c7de01da2b2ac2155b5fb
Aleksandar Lalov <alex.lalov04@gmail.com>
2023-03-28 18:07:49 +0300

    Adding seeder to the project

--- Kanban/Data/DbInitializer.cs
+++ Kanban/Data/DbInitializer.cs
@@ -1,5 +1,6 @@
 ﻿
 using Kanban.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Kanban.Data
 {
@@ -14,49 +15,136 @@ namespace Kanban.Data
                 return;
             }
 
-            context.Users.AddRange(
-                new User
+            IdentityRole[] roles = new IdentityRole[]
+            {
+                new IdentityRole
                 {
-                    Id = "07387941-53df-4db7-9223-78afcbb307c70",
-                    FirstName = "Alex",
-                    LastName = "Lalov",
-                    Email = "admin@admin.com",
-                    UserName= "admin",
+                    Id = "4af8f32f-1c3c-43b0-9f5a-216f3df61618",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
                 },
-                new User
-                {
-                    Id = "c146d32e-7362-4402-8271-d942f2593bd4",
-                    FirstName = "Alex",
-                    LastName = "Lalov",
-                    Email = "developer@developer.com",
-                    UserName= "developer"
-                },
-                new User
-                {
-                    Id = "c2fb1f90-0cc6-4484-8e6e-5b371a0090fe",
-                    FirstName = "Alex",
-                    LastName = "Lalov",
-                    Email = "client@client.com",
-                    UserName = "client"
-                }
-            );
 
+                new IdentityRole
+                {
+                    Id = "6bd33099-ebce-44d7-90dc-87312c20ee64",
+                    Name = "Client",
+                    NormalizedName = "CLIENT"
+                },
+                new IdentityRole
+                {
+                    Id = "c0027093-a4d1-4630-8458-f1195bc5e637",
+                    Name = "Developer",
+                    NormalizedName = "DEVELOPER"
+                }
+            };
+
+            context.Roles.AddRange(roles);
             context.SaveChanges();
 
-            context.Tasks.AddRange(
+            //Users
+            User[] users = new User[]
+            {
+                new User
+                {
+                    Id = "7f0aca23-1fc5-447a-88f8-a078f2ab580c",
+                    UserName = "admin",
+                    FirstName = "Admin",
+                    LastName = "Admin",
+                    NormalizedUserName = "ADMIN",
+                    Email = "admin@admin.com",
+                    NormalizedEmail = "ADMIN@ADMIN.COM"
+                },
+                new User
+                {
+                    Id = "d504e155-5a72-4da9-8872-71876610436a",
+                    UserName = "client",
+                    FirstName = "Client",
+                    LastName = "Client",
+                    NormalizedUserName = "CLIENT",
+                    Email = "client@client.com",
+                    NormalizedEmail = "CLIENT@CLIENT.COM"
+                },
+
+                new User
+                {
+                    Id = "99f7484c-d997-4b6e-87d4-27d59f5c7c6e",
+                    UserName = "developer",
+                    FirstName = "Developer",
+                    LastName = "Developer",
+                    NormalizedUserName = "DEVELOPER",
+                    Email = "developer@developer.com",
+                    NormalizedEmail = "DEVELOPER@DEVELOPER.COM"
+                },
+
+            };
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            PasswordHasher<User> hasher = new();
+            users[0].PasswordHash = hasher.HashPassword(users[0], "Admin1!");
+            users[1].PasswordHash = hasher.HashPassword(users[1], "Client1!");
+            users[1].PasswordHash = hasher.HashPassword(users[2], "Developer1!");
+
+            //UserRoles
+            IdentityUserRole<string>[] userRoles = new IdentityUserRole<string>[]
+            {
+                new IdentityUserRole<string>
+                {
+                    UserId = users[0].Id,
+                    RoleId = roles[0].Id
+                },
+
+                new IdentityUserRole<string>
+                {
+                    UserId = users[1].Id,
+                    RoleId = roles[1].Id
+                },
+                new IdentityUserRole<string>
+                {
+                    UserId = users[2].Id,
+                    RoleId = roles[2].Id
+                }
+            };
+
+            context.UserRoles.AddRange(userRoles);
+            context.SaveChanges();
+
+            Models.Task[] tasks = new Models.Task[]
+            {
                 new Models.Task
                 {
-                    Id = 1,
                     Title = "Fix UI",
-                    CreatorId = "07387941-53df-4db7-9223-78afcbb307c70",
-                    ExecutorId = "c146d32e-7362-4402-8271-d942f2593bd4",
+                    CreatorId = users[0].Id,
                     CreatedDate = DateTime.Now,
                     ExpectedEndDate = DateTime.Now.AddDays(7),
                     Description = "Fix a graphic mistake when clicking the login button",
                     Status = Models.Task.Statuses.Open,
-                }
+                },
+                new Models.Task
+                {
+                    Title = "Fix Backend",
+                    CreatorId = users[0].Id,
+                    ExecutorId = users[2].Id,
+                    CreatedDate = DateTime.Now,
+                    ExpectedEndDate = DateTime.Now.AddDays(7),
+                    Description = "Fix message coming from error when entering invalid data",
+                    Status = Models.Task.Statuses.InProgress,
+                },
+                new Models.Task
+                {
+                    Title = "Fix UX",
+                    CreatorId = users[0].Id,
+                    ExecutorId = users[2].Id,
+                    CreatedDate = DateTime.Now,
+                    ExpectedEndDate = DateTime.Now.AddDays(7),
+                    Description = "The login button is too far to be easily visible and accessable to users",
+                    Status = Models.Task.Statuses.Finished,
+                },
+            };
 
-            );
+            context.Tasks.AddRange(tasks);
+            context.SaveChanges();
 
         }
     }
---
+++ Kanban/Data/DbInitializerExtension.cs
@@ -0,0 +1,26 @@
+﻿namespace Kanban.Data
+{
+    public static class DbInitializerExtension
+    {
+        public static IApplicationBuilder Seed(this IApplicationBuilder builder)
+        {
+            ArgumentNullException.ThrowIfNull(builder, nameof(builder));
+
+            using var scope = builder.ApplicationServices.CreateScope();
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                DbInitializer.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                ILogger<Program> logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured seeding users.");
+            }
+
+            return builder;
+        }
+    }
+}
